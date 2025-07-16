56f5beb97bfd838362c5fe83b83e9b762dc56435
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2025-07-16 22:03:35 +0700

    chore: add frontend url env and clean unused vars

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
